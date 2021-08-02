492c11359e9a5800df419ddac1c34cc506a7c7de
Ben Shand <benjaminshand101@gmail.com>
2021-08-03 07:54:18 +1200

    Added selling.hmtl

--- Website2021/main.py
+++ Website2021/main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template,url_for, request
+from flask import Flask, render_template, url_for, request
 
 from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import or_
@@ -10,22 +10,24 @@ db = SQLAlchemy(app)
 
 import models
 
+
 @app.route("/")
 def home():
     return render_template("home.html")
 
 
-#route for about page
+# route for about page
 @app.route("/about")
 def about():
     return render_template("about.html")
+
 
 @app.route("/nav")
 def nav(womens_all):
     ya_dig = models.Products.query.order_by(models.Products.id).first()
 
     all_products = models.Products.query.order_by(models.Products.id).all()
-    return render_template("nav.html",all_products=all_products,ya_dig=ya_dig)
+    return render_template("nav.html", all_products=all_products, ya_dig=ya_dig)
 
 
 @app.route("/our_stores")
@@ -52,62 +54,85 @@ def refund_policy():
 def work_with_us():
     return render_template("work_with_us.html")
 
-#shop landing page
+
+# shop landing page
 @app.route("/shop")
 def shop():
     all_products = models.Products.query.order_by(models.Products.id).all()
-    return render_template("shop.html",all_products=all_products)
+    return render_template("shop.html", all_products=all_products)
+
 
 #######################################
 @app.route("/shop/mens")
 def mens_filter():
-    mens_all = models.Products.query.order_by(models.Products.gender).filter_by(gender="mens").all()
-    #filter_first = models.Products.query.filter_by(gender="mens").first()
+    mens_all = (
+        models.Products.query.order_by(models.Products.gender)
+        .filter_by(gender="mens")
+        .all()
+    )
+    # filter_first = models.Products.query.filter_by(gender="mens").first()
     return render_template("mens_filter.html", mens_all=mens_all)
 
-@app.route('/shop/mens/<product_type>')
+
+# mens shopping filter for differnt clothing types
+@app.route("/shop/mens/<product_type>")
 def product_type_filter(product_type):
-    product_type= models.Products.query.order_by(models.Products.product_type).filter_by(product_type=product_type).filter_by(gender="mens").all()
-    return render_template('product_type_filter.html', product_type=product_type)
+    product_type = (
+        models.Products.query.order_by(models.Products.product_type)
+        .filter_by(product_type=product_type)
+        .filter_by(gender="mens")
+        .all()
+    )
+    return render_template("product_type_filter.html", product_type=product_type)
+
 
 @app.route("/shop/womens")
 def womens_shop_filter():
-    womens_all = models.Products.query.order_by(models.Products.gender).filter_by(gender="womens").all()
-    #filter_first = models.Products.query.filter_by(gender="mens").first()
+    womens_all = (
+        models.Products.query.order_by(models.Products.gender)
+        .filter_by(gender="womens")
+        .all()
+    )
+    # filter_first = models.Products.query.filter_by(gender="mens").first()
     return render_template("womens_filter.html", womens_all=womens_all)
 
-@app.route('/shop/womens/<product_type>')
+
+# womens shopping filter for differnt clothing types
+@app.route("/shop/womens/<product_type>")
 def womens_product_type_filter(product_type):
-    product_type= models.Products.query.order_by(models.Products.product_type).filter_by(product_type=product_type).filter_by(gender="womens").all()
-    return render_template('product_type_filter.html', product_type=product_type)
+    product_type = (
+        models.Products.query.order_by(models.Products.product_type)
+        .filter_by(product_type=product_type)
+        .filter_by(gender="womens")
+        .all()
+    )
+    return render_template("product_type_filter.html", product_type=product_type)
 
 
-
-
-@app.route('/shop/brands/<brand>')
+@app.route("/shop/brands/<brand>")
 def separate_brands(brand):
-    all_brands= models.Products.query.filter_by(brand=brand).all()
-    return render_template('separate_brands.html', all_brands=all_brands)
+    all_brands = models.Products.query.filter_by(brand=brand).all()
+    return render_template("separate_brands.html", all_brands=all_brands)
 
 
-
-#seprate products pages
-@app.route('/products/<id>')
+# seprate products pages
+@app.route("/products/<id>")
 def separate_products(id):
-    sep= models.Products.query.filter_by(id=id).first_or_404()
-    return render_template('separate_products.html', sep=sep)
+    sep = models.Products.query.filter_by(id=id).first_or_404()
+    return render_template("separate_products.html", sep=sep)
 
 
-#route for sell page
+# route for sell page
 @app.route("/sell")
 def sell():
     return render_template("sell.html")
 
 
-#reroutes 404 errrors
+# reroutes 404 errrors
 @app.errorhandler(404)
 def page_not_found(e):
-    return render_template('404.html'), 404
+    return render_template("404.html"), 404
+
 
 if __name__ == "__main__":
     app.run(debug=True)
